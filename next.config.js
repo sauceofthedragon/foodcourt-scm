e1b4810cd0cef34134e93148af82b1e5d904272a
sauceofthedragon <sauceofthedragon@gmail.com>
2026-06-16 06:06:49 +0900

    add supabase config

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {}
-
+js/** @type {import('next').NextConfig} */
+const nextConfig = {
+  env: {
+    NEXT_PUBLIC_SUPABASE_URL: 'https://ubhirekjjnlrwiwrqaqs.supabase.co',
+    NEXT_PUBLIC_SUPABASE_ANON_KEY: 'sb_publishable_AnrJbyHu8Vhq5gByruMs7g_WwfIRVpt,
+  },
+}
 module.exports = nextConfig
